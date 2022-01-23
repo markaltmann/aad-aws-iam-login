1847576493b0dff1c06423621bada5a9b2607da8
Mark Altmann <mark@altmann.it>
2022-01-23 16:44:15 +0100

    Add the diagrams test with a minimum and running poetry

--- diagrams/overview.py
+++ diagrams/overview.py
@@ -1,3 +1,27 @@
 # This is the overview diagrams file in python
+from diagrams import Cluster, Diagram, Edge
+# AWS Imports
+from diagrams.aws.security import IAM, IdentityAndAccessManagementIamTemporarySecurityCredential as TSC
+# Azure Imports
+from diagrams.azure.identity import ActiveDirectory
+# Generic Imports
+from diagrams.onprem.auth import Oauth2Proxy
+from diagrams.onprem.client import Client
 
-import diagrams
+with Diagram(name="Azure AAD login for AWS IAM", show=False, direction="LR"):
+    with Cluster("Azure"):
+        AAD = ActiveDirectory("Azure AAD")
+
+    with Cluster("AWS"):
+        IAM = IAM("IAM")
+        STS_Token = TSC("STS Token")
+
+    with Cluster("OnPrem"):
+        OAuth = Oauth2Proxy("OAUTH Proxy")
+        Client = Client("User Machine")
+
+    Client >> AAD
+    AAD >> OAuth
+    IAM << OAuth
+    STS_Token << IAM
+    Client << STS_Token
